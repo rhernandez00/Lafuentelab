b612edf487be2a7b1134e2527a7135f3aabc7533
Raúl Hernández <raul@lafuentelab.org>
2017-11-30 16:22:39 -0600

    getFileMetadata
    Agrega el número de ensayos útiles y si la neurona debe descartarse

--- getFileMetadata.m
+++ getFileMetadata.m
@@ -1,5 +1,9 @@
-function tbl = getFileMetadata(fileName,folder)
+function tbl = getFileMetadata(fileName,varargin)
 % gets metadata from a file and gives it back as a table. One row for each neuron
+
+% default variables
+analysisVar = getArgumentValue('analysisVar' ,'anguloInicio', varargin{:});
+folder = getArgumentValue('folder' ,'C:\Users\Raul\Desktop\registros\recordings\', varargin{:});
 
 load([folder,fileName]); %loads e
 neuron = fields(e.spikes); %gets the available neurons
@@ -10,12 +14,14 @@ coordenadaY = e.canulas.coordenadas(2) * ones(length(neuron),1);
 coordenadaZ = e.canulas.coordenadas(3) * ones(length(neuron),1);
 contacto = e.canulas.contacto * ones(length(neuron),1);
 
+%orientation
 orientacion = cell(length(neuron),1);
-[orientacion{:}] = deal(e.canulas.orientacion);
+[orientacion{:}] = deal(e.canulas.orientacion); %this function is used to assign
+%one string to various cells
 
 monkey = cell(length(neuron),1);
 [monkey{:}] = deal(fileName(1));
-
+monkey = categorical(monkey);
 dateRegistered = cell(length(neuron),1);
 [dateRegistered{:}] = deal(strtok(e.Fecha));
 
@@ -31,9 +37,16 @@ profundidadZ = e.electrodos.profundidad(3) * ones(length(neuron),1);
 nTrials = length(e.trial)* ones(length(neuron),1);
 
 
+%calculates the number of trials for a specific type of analysis var
+windowIndx = 1;
+discard = zeros(length(neuron),1);
+for j = 1:length(neuron)
+    [x,~,discard(j)] = getColumn(fileName,neuron{1},windowIndx);
+end
+analysisVarTrials = length(x) * ones(length(neuron),1);
+
 %assigns the data to a table
 tbl = table(dateRegistered,neuron,coordenadaX,coordenadaY,coordenadaZ,...
     contacto,cortezaX,cortezaY,cortezaZ,...
     orientacion,profundidadX,profundidadY,profundidadZ,...
-    nTrials,monkey,file);
-
+    nTrials,monkey,file,analysisVarTrials,discard);
